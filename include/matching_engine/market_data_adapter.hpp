87b71bfc1065cc62051d860c031daaf05f16b43e
Divyanshu <bautocrats@gmail.com>
2026-08-28 23:14:38 +0530

    feat: validate market data before sequencing

--- include/matching_engine/market_data_adapter.hpp
+++ include/matching_engine/market_data_adapter.hpp
@@ -2,6 +2,7 @@
 #define MATCHING_ENGINE_MARKET_DATA_ADAPTER_HPP
 
 #include "matching_engine/command.hpp"
+#include "matching_engine/gateway.hpp"
 #include "matching_engine/market_data_protocol.hpp"
 
 #include <expected>
@@ -17,12 +18,19 @@ enum class MarketDataAdaptError : std::uint8_t {
 
 class MarketDataAdapter {
 public:
+  explicit MarketDataAdapter(GatewayValidator gateway) noexcept : gateway_{std::move(gateway)} {}
+
   [[nodiscard]] std::expected<SequencedCommand, MarketDataAdaptError>
   adapt(const MarketDataMessage& message) noexcept;
+  [[nodiscard]] GatewayRejectReason last_gateway_reject_reason() const noexcept {
+    return last_gateway_reject_reason_;
+  }
 
 private:
+  GatewayValidator gateway_;
   Sequencer sequencer_{};
   std::uint64_t previous_market_data_sequence_{};
+  GatewayRejectReason last_gateway_reject_reason_{GatewayRejectReason::none};
 };
 
 } // namespace matching_engine
